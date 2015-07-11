3a289857c4540f284ac138da0842c86825066e47
getabetterpic <dan@danandjensmith.com>
2015-07-11 14:11:27 -0400

    Including semantic-ui and worked on some entry stuff

--- app/components/budget-entries.js
+++ app/components/budget-entries.js
@@ -2,12 +2,48 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   categories: undefined,
-  rootCategories: Ember.computed('model.@each.ancestry', function() {
+  currentDate: moment(),
+  selectedDate: moment(),
+  rootCategories: Ember.computed('model.@each.category', function() {
     return this.get('store').filter('category', function(category) {
       return category.get('ancestry') === null;
     });
   }),
+  budgetEntries: Ember.computed('model.@each.category', 'selectedDate', function() {
+    var selectedDate = this.get('selectedDate');
+    var entries = this.get('store').filter('budgetEntry', function(entry) {
+      return moment(entry.get('date')).isSame(selectedDate, 'month');
+    });
+    console.log(entries);
+    return entries;
+  }),
+  entryForCategory: function(category) {
+    console.log(this.get('budgetEntries'));
+    var self = this;
+    var currentEntry = null;
+    this.get('budgetEntries').forEach(function(entry) {
+      if (
+        moment(entry.get('date')).isSame(self.get('selectedDate'), 'month') &&
+        entry.get('category') === category.get('name')
+      ) {
+        currentEntry = entry;
+      }
+    });
+    return currentEntry;
+  },
   didInsertElement() {
     this.set('categories', this.get('store').find('category'));
+  },
+  actions: {
+    nextMonth() {
+      var currentSelected = this.get('selectedDate');
+      var newSelected = currentSelected.add('1', 'months');
+      this.set('selectedDate', newSelected);
+    },
+    previousMonth() {
+      var currentSelected = this.get('selectedDate');
+      var newSelected = currentSelected.subtract('1', 'months');
+      this.set('selectedDate', newSelected);
+    }
   }
 });
